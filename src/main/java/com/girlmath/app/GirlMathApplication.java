f952e9f0e78d84c0c3d22066b5cc8aabd2fe2a56
Jennifer Tellez Vera <102021487+jennyfertellez@users.noreply.github.com>
2026-04-28 15:25:50 -0700

    initial project setup: Spring Boot scaffold

---
+++ src/main/java/com/girlmath/app/GirlMathApplication.java
@@ -0,0 +1,13 @@
+package com.girlmath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlMathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GirlMathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/girlmath/app/GirlMathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girlmath.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlMathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
